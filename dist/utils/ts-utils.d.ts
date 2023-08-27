7e4b407b2bf839f86e195d046e3e9b24d864f6d4
Nyanator <asaharatoru@gmail.com>
2023-08-27 23:14:14 +0900

    可読性のために不要なものを削除

--- dist/utils/ts-utils.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * 宣言的null例外サポートです。
- * nullableを許容しないコンテキストで非nullableへの変換に使います。
- * @param value 検査対象
- * @returns 非nullableな値
- */
-export declare const assertNotNull: <T>(value: T | null | undefined) => T;
